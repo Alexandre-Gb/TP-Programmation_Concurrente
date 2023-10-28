8573d96285bce0e001b86cb2e3e15c1af3603a44
Alexandre-Gb <giboz.alexandre@gmail.com>
2023-10-28 15:30:42 +0200

    Exo3 (10/11 tests)

--- TP 7/src/ThePriceIsRight.java
+++ TP 7/src/ThePriceIsRight.java
@@ -5,42 +5,68 @@ import java.util.concurrent.locks.ReentrantLock;
 public class ThePriceIsRight {
   private final int price;
   private final int nbThreads;
+  private boolean hasBeenInterrupted;
   private final LinkedHashMap<Thread, Integer> propositions = new LinkedHashMap<>();
   private final ReentrantLock lock = new ReentrantLock();
   private final Condition notAllPropositions = lock.newCondition();
 
   public ThePriceIsRight(int price, int nbThreads) {
-    if (price < 0 || nbThreads < 1) {
-      throw new IllegalArgumentException();
-    }
-
-    this.price = price;
-    this.nbThreads = nbThreads;
-  }
-
-  public boolean propose(int proposition) throws InterruptedException {
     lock.lock();
     try {
-      propositions.put(Thread.currentThread(), proposition);
-      notAllPropositions.signalAll();
-      while (propositions.size() != nbThreads) {
-        notAllPropositions.await();
+      if (price < 0 || nbThreads < 1) {
+        throw new IllegalArgumentException();
       }
 
-      int closest = Integer.MAX_VALUE;
-      for (int p : propositions.values()) {
-        if (Math.abs(p - price) < Math.abs(closest - price)) {
-          closest = p;
-        }
-      }
-
-      return closest == proposition;
+      this.price = price;
+      this.nbThreads = nbThreads;
     } finally {
       lock.unlock();
     }
   }
 
-  public static void main(String[] args) {
+  public boolean propose(int proposition) {
+    lock.lock();
+    try {
+      if (proposition < 0) {
+        throw new IllegalArgumentException();
+      }
 
+      if (propositions.containsKey(Thread.currentThread()) || propositions.size() == nbThreads) {
+        return false;
+      }
+
+      propositions.put(Thread.currentThread(), proposition);
+      notAllPropositions.signalAll();
+
+      while (!hasBeenInterrupted && propositions.size() != nbThreads) {
+        notAllPropositions.await();
+      }
+
+      var closestPrice = Integer.MAX_VALUE;
+      for (int p : propositions.values()) {
+        if (distance(p) < distance(closestPrice)) {
+          closestPrice = p;
+        }
+      }
+
+      return (closestPrice == proposition &&
+              propositions.values().stream()
+                      .filter(p -> p == proposition)
+                      .count() == 1);
+    } catch (InterruptedException e) {
+      if (nbThreads != 1) {
+        hasBeenInterrupted = true;
+        propositions.remove(Thread.currentThread());
+        notAllPropositions.signalAll();
+      }
+
+      return false;
+    } finally {
+      lock.unlock();
+    }
+  }
+
+  private int distance(int proposition) {
+    return Math.abs(proposition - price);
   }
 }
